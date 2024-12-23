a88c075d91b0a064ba2748bd92e38898f469c74f
Jude Pramodh <judepramodh@gmail.com>
2024-12-23 20:06:13 +0000

    Navigation Setup

--- app/src/main/java/com/example/quicktalk/Screens/SignUpScreen.kt
+++ app/src/main/java/com/example/quicktalk/Screens/SignUpScreen.kt
@@ -11,26 +11,24 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.navigation.NavController
 import com.example.quicktalk.DestinationScreen
 import com.example.quicktalk.QTViewModel
+import com.example.quicktalk.R
 
 @Composable
 fun SignUpScreen(navController: NavController, vm: QTViewModel) {
-
-    Box(modifier = Modifier.fillMaxSize()){
-
-        Column (modifier = Modifier.fillMaxSize().wrapContentHeight().verticalScroll(
-            rememberScrollState()
-        )){
-
-            Image(painter = , contentDiscription=null)
-
-
-
+    Box(modifier = Modifier.fillMaxSize()) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .verticalScroll(rememberScrollState())
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.quicktalk2),
+                contentDescription = null
+            )
         }
-
     }
-
-
 }
